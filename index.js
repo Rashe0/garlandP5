fa25781da57363903cbc7983a4fbb114c5d0f2c8
Maciej <maciejkry@gmail.com>
2020-04-15 16:18:33 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/garlandp5', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MondoBD', err))  
+
+app.get('/',(req, res) => {
+    res.send('you connected, grats')
+})
+
+app.listen(5000, () => console.log('Listening on port 5000...'))
